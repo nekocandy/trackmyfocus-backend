90b5935403e749efa6796f47b827760517b09fc2
Piyush <bhangalepiyush@gmail.com>
2022-12-18 11:19:24 +0530

    use env for port

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const _checkAuth = auth({
 app.use('/api', ApiRoute);
 app.use('/data', DataRoute);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
