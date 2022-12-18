ae06cd945b9ffa83dd9cb684241e3d65201becec
Piyush <bhangalepiyush@gmail.com>
2022-12-18 10:38:39 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
+import { config } from 'dotenv';
+
 import * as express from 'express';
+import { auth } from 'express-oauth2-jwt-bearer';
+
+import ApiRoute from './routes/ApiRoute';
+
+config();
 
 const app = express();
+
+// eslint-disable-next-line max-len
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/naming-convention, no-underscore-dangle
+const _checkAuth = auth({
+  audience: process.env.AUDIENCE!!,
+  issuer: process.env.ISSUER_BASE_URL!!,
+});
+
+app.use('/api', ApiRoute);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
